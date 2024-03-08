e8c965f9e38ce4d7a6b75662996bf770cd1aebcb
BenyaminMusavi <beni97d@gmail.com>
2024-03-09 00:02:58 +0330

    Add project files.

---
+++ StudentsClient.Domain/Model/StudentModel.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentsClient.Domain.Model;
+
+public record class StudentModel
+{
+    public int Id { get; init; }
+    public string StudentNumber { get; init; }
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public string Description { get; init; }
+    public List<string> PhoneNumbers { get; init; } = new List<string>();
+}
+
